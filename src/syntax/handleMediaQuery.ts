4b7223aad6bd9b3c6aadb67bdc2f4c082bc763d0
jn <jnoncode@gmail.com>
2024-11-27 17:29:17 +0900

    feat: add handleMediaQuery

---
+++ src/syntax/handleMediaQuery.ts
@@ -0,0 +1,33 @@
+import { MediaQueryInput, StyleObject } from "../types";
+import { isRequiredUnits } from "./checkUnits";
+
+export const breakpoints: MediaQueryInput = {
+  "@mobile": "(max-width: 768px)",
+  "@desktop": "(min-width: 769px)",
+};
+
+export const handleMediaQuery = (
+  mqType: string,
+  style: StyleObject,
+  className: string
+) => {
+  let mediaQueryString = `@media ${mqType} { .${className} { `;
+  let resultString = "";
+
+  for (const key in style) {
+    if (style.hasOwnProperty(key)) {
+      const styleKey = key.replace(/([A-Z])/g, "-$1").toLowerCase();
+      const styleItem = style[key];
+      if (typeof styleItem === "number") {
+        resultString += `${styleKey}: ${styleItem}${
+          isRequiredUnits(styleKey) ? "px" : ""
+        }; `;
+      } else {
+        resultString += `${styleKey}: ${styleItem}; `;
+      }
+    }
+    mediaQueryString += resultString;
+  }
+  mediaQueryString += `} };`;
+  return mediaQueryString;
+};
