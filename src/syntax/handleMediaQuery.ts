7374b70f5ce4d08b288c9b64e369188807ca4e82
jn <jnoncode@gmail.com>
2024-11-29 20:32:19 +0900

    refactor: handleMediaQuery

--- src/syntax/handleMediaQuery.ts
+++ src/syntax/handleMediaQuery.ts
@@ -1,31 +1,70 @@
 import { isRequiredUnits } from "./checkUnits";
-import { MediaQueryInput, StyleInputType } from "../types";
+import { StyleType } from "../types";
+import { isPseudoElements } from "./checkPseudoElements";
+import { handlePseudoElements } from "./handlePseudoElements";
+import { handleCombinators } from "./handleCombinator";
+import { handleGeneralCSS } from "./handleGeneralCSS";
 
 export const handleMediaQuery = (
   mediaQueryKey: string,
-  style: StyleInputType,
-  className: string
+  inputStyle: StyleType,
+  hash: string
 ) => {
-  let mediaQueryString = `@media ${mediaQueryKey} { .${className} { `;
-  let resultString = "";
+  let mediaQueryString = `@media ${mediaQueryKey} {`;
+  const resultOfCSS = [];
+  const resultOfGeneralCSS = [];
 
-  for (const key in style) {
-    if (style.hasOwnProperty(key)) {
-      const styleKey = key.replace(/([A-Z])/g, "-$1").toLowerCase();
-      const styleItem = style[key];
-      if (typeof styleItem === "number") {
-        resultString += `${styleKey}: ${styleItem}${
-          isRequiredUnits(styleKey) ? "px" : ""
-        }; `;
-      } else {
-        resultString += `${styleKey}: ${styleItem}; `;
+  for (const key in inputStyle) {
+    if (inputStyle.hasOwnProperty(key)) {
+      const itemClassName = `${key}-${hash}`;
+      const itemStyle = inputStyle[key] as any;
+      let bufferGeneralCSS = "";
+
+      for (const elementKey in itemStyle) {
+        const elementStyle = itemStyle[elementKey];
+        // console.log(elementStyle);
+        switch (true) {
+          case isPseudoElements(elementKey): // 가상요소
+            const pResult = handlePseudoElements(
+              elementKey,
+              elementStyle,
+              itemClassName
+            );
+            resultOfCSS.unshift(pResult);
+            break;
+
+          case /^[>+~ ]/.test(elementKey): // 조합자
+            const cResult = handleCombinators(
+              elementKey,
+              elementStyle,
+              itemClassName
+            );
+            resultOfCSS.unshift(cResult);
+            break;
+
+          default: // 그외 일반 스타일
+            bufferGeneralCSS += handleGeneralCSS(elementKey, elementStyle);
+            break;
+        }
       }
+
+      resultOfGeneralCSS.push({
+        className: itemClassName,
+        style: `.${itemClassName} { ${bufferGeneralCSS} }`,
+      });
     }
-    mediaQueryString += resultString;
   }
-  mediaQueryString += `} }`;
+
+  const result = [...resultOfGeneralCSS, ...resultOfCSS];
+
+  for (let el of result) {
+    mediaQueryString += el.style;
+  }
+
+  mediaQueryString += "}";
+
   return {
-    className: `@media ${mediaQueryKey} .${className}`,
+    className: `@media-${mediaQueryKey}-${hash}`,
     style: mediaQueryString,
   };
 };
