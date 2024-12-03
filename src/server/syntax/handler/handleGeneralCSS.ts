cdb7358037f7cd60d32df812c1e8c012e0684fe5
vectordxy <jnoncode@gmail.com>
2024-12-03 10:21:51 +0900

    refactor: remove unuse input

--- src/server/syntax/handler/handleGeneralCSS.ts
+++ src/server/syntax/handler/handleGeneralCSS.ts
@@ -2,8 +2,7 @@ import { isRequiredUnits } from "../checker/checkUnits";
 
 export const handleGeneralCSS = (
   elementKey: string,
-  inputStyle: string | number,
-  className: string
+  inputStyle: string | number
 ) => {
   let resultString = "";
   const styleKey = elementKey.replace(/([A-Z])/g, "-$1").toLowerCase();
