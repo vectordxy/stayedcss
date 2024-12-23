3e75c6b025e924d6caffda5946f93e2033310fa5
jin <kjin.code@gmail.com>
2024-12-23 09:08:51 +0900

    refactor: remove keyframes logic

--- src/styles/generate/generateStylesFile.ts
+++ src/styles/generate/generateStylesFile.ts
@@ -1,12 +1,5 @@
-import {
-  BreakPoints,
-  Json,
-  Keyframes,
-  MainInput,
-  StyleObjectItem,
-  StylesForProxy,
-} from "../../types";
-import { handleKeyframes, handleMediaQuery } from "..";
+import { BreakPoints, Json, MainInput, StyleObjectItem } from "../../types";
+import { handleMediaQuery } from "..";
 import { writeStaticCSS, writeStaticDarkModeCSS } from "./utils";
 import { generateStylesWithSyntax } from "./generateStylesWithSyntax";
 import { defaultBreakpoints } from "./utils/common/handleBreakpoints";
@@ -14,27 +7,17 @@ import {
   formatComponentId,
   toLowerCaseComponentId,
 } from "./utils/common/handleComponentId";
-import { loadBreakpoints } from "./utils/files/loadBreakpoints";
 
 export const getStaticStyles = (
   input: MainInput,
   inputScreenMode: "default" | "dark"
 ) => {
-  // let inputBreakpoints = loadBreakpoints();
   let inputBreakpoints: BreakPoints = defaultBreakpoints;
-  let inputKeyframes: Keyframes = {};
 
   const componentClassName = formatComponentId(input.componentId as string);
   const componentFileRootName = toLowerCaseComponentId(
     input.componentId as string
   );
-
-  // if (config) {
-  //   const { breakpoints, keyframes } = config;
-
-  //   inputBreakpoints = breakpoints || defaultBreakpoints;
-  //   inputKeyframes = keyframes || {};
-  // }
 
   const styleData = Object.entries(input).filter(
     ([key]) => key !== "componentId"
@@ -42,15 +25,6 @@ export const getStaticStyles = (
 
   let result: Json[] = [];
 
-  const stylesForProxy: StylesForProxy = {};
-  let keyframesResult: Json[] = [];
-
-  // 키프레임 애니메이션 처리
-  if (inputKeyframes) {
-    keyframesResult = handleKeyframes(inputKeyframes);
-  }
-
-  // 애니메이션 처리
   for (let item of styleData) {
     const itemName = item[0];
     const itemStyle = item[1] as unknown as StyleObjectItem;
@@ -58,8 +32,6 @@ export const getStaticStyles = (
       inputScreenMode === "default"
         ? `${itemName}-${componentClassName}`
         : `dark .${itemName}-${componentClassName}`;
-
-    stylesForProxy[itemName] = "";
 
     if (itemName in inputBreakpoints) {
       // 미디어쿼리
@@ -76,10 +48,9 @@ export const getStaticStyles = (
         ...result,
       ];
     }
-    stylesForProxy[itemName] = itemClassName;
   }
 
-  const styleResult = [...keyframesResult, ...result];
+  const styleResult = [...result];
 
   if (inputScreenMode === "default") {
     writeStaticCSS(styleResult, componentFileRootName);
